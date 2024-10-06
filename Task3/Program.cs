0c2f5c74aad47a414ea0684c8ef5220b4664b593
max <m.d.mihailik@gmail.com>
2024-10-06 23:31:00 +0300

    fix: minor fixes

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,22 +1,30 @@
 using Task3.ImplementationOfAllStuff;
 using Task3.TaskModel;
 
-var handler = new ConsoleMessageHandler();
-var processor = new MessageProcessor(handler, batchSize: 5);
+namespace Task3;
 
-MessageProcessor sender = processor;
-MessageProcessor messageProcessor = processor;
-
-var cancellationToken = new CancellationTokenSource();
-
-Task processingTask = messageProcessor.ProcessAsync(cancellationToken.Token);
-
-IEnumerable<Message> messages = Enumerable.Range(1, 50).Select(i => new Message($"Title {i}", $"Text {i}"));
-
-await Parallel.ForEachAsync(messages, cancellationToken.Token, async (message, token) =>
+public static class Program
 {
-    await sender.SendAsync(message, token).ConfigureAwait(false);
-}).ConfigureAwait(false);
+    public static async void Main()
+    {
+        var handler = new ConsoleMessageHandler();
+        var processor = new MessageProcessor(handler, batchSize: 5);
 
-messageProcessor.Complete();
-await processingTask.ConfigureAwait(false);
+        MessageProcessor sender = processor;
+        MessageProcessor messageProcessor = processor;
+
+        var cancellationToken = new CancellationTokenSource();
+
+        Task processingTask = messageProcessor.ProcessAsync(cancellationToken.Token);
+
+        IEnumerable<Message> messages = Enumerable.Range(1, 50).Select(i => new Message($"Title {i}", $"Text {i}"));
+
+        await Parallel.ForEachAsync(messages, cancellationToken.Token, async (message, token) =>
+        {
+            await sender.SendAsync(message, token).ConfigureAwait(false);
+        }).ConfigureAwait(false);
+
+        messageProcessor.Complete();
+        await processingTask.ConfigureAwait(false);
+    }
+}
